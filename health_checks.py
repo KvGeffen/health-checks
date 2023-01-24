fde3bfa9da240a271be90254b9e487478c65eb22
KvGeffen <46600718+KvGeffen@users.noreply.github.com>
2023-01-24 11:01:21 +0100

    Update and rename all_checks.py to health_checks.py
    add cpu test and rename file

--- health_checks.py
+++ health_checks.py
@@ -27,6 +27,12 @@ def check_root_full():
     """
     return check_disk_full(disk='/',min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+    """
+    Returns True if the cpu is having too much usage, False otherwise.
+    """
+    return psutil.cpu_percent(1) > 75
+
 def main():
     checks=[
         (check_reboot, "Pending Reboot"),
